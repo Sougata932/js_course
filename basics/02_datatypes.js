464b5376e2f2414d773a0ddae861c66d918be5c3
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-06-29 15:17:51 +0000

    container config done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS codes as newer version
+//alert(3+3) //will show 6 in browser, not here
+console.log(3+3)
+let name="Sougata"
+let age=20
+let scene=true
+let temp=null
+let string;
+console.log(typeof string)
+console.log(typeof temp)
+// null is a object type 
+
---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let a="33a"
+let b= "Ream"
+let c= "33"
+console.log(typeof(score));
+let valueConvert1 = Number(a)
+
+let valueConvert2 = Number(b)
+let valueConvert3 = Number(c)
+console.table([typeof valueConvert1,typeof valueConvert2, typeof valueConvert3])
+console.table([valueConvert1, valueConvert2, valueConvert3]);
+
+//NaN => no a number
+
+
+
