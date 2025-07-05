9c920923d98f96d02a2a7d85ed9e810833a8a3be
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-07-05 14:05:14 +0000

    container config done

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -10,6 +10,18 @@ console.table([typeof valueConvert1,typeof valueConvert2, typeof valueConvert3])
 console.table([valueConvert1, valueConvert2, valueConvert3]);
 
 //NaN => no a number
+//True=1 False =0
+//"" => False
+//"something" => True
+let isLogin=1
+let conv=Boolean(isLogin)
+console.log(conv);
+let no = 23
+let str = String(no)
+console.log(str);
+console.log(typeof str);
+
+
 
 
 
