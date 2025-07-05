f783660d64010072b4797024d75f07b165dc0ebb
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-07-05 15:01:43 +0000

    done

---
+++ basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1); //will take the value
+console.log(null==0); //equality check
+console.log(null>=0); //comparison operators treats null as 0
+console.log(undefined>=0);
+
+console.log("2"==2) //only checks the value
+console.log("2"===2); //also checks the data type
+
+
+
+
