e2f5f1c0dea0c5b0fd178d145945047be82edcf1
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-07-10 18:14:54 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID=12345 //we use to declare constant term 
+let accountName = "Sougata" //mostly used to declare a variable
+var accountEmail = "smondal@gmail.com"  //old way to declare a variable
+accountPassword = 8932 //will take it as a variable, but bad practice
+
+console.log(accountID)
+console.table([accountEmail, accountID, accountName, accountPassword])
+
