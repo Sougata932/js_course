38afd3fd65bf4974ac3e1095585e94ac09c3f1d2
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-07-11 18:03:32 +0000

    done

---
+++ 02_basics/02_array.js
@@ -0,0 +1,10 @@
+const marvel = ['ironman', 'thor', 'hulk']
+const dc = ['batman', 'superman', 'flash']
+
+// marvel.push(dc) //will add dc as an array
+// console.log(marvel);
+
+// console.log(marvel.concat(dc));
+ marvel.concat(dc)
+ console.log(marvel.concat(dc));
+ 
--- basics/01_variables.js
+++
@@ -1,8 +0,0 @@
-const accountID=12345 //we use to declare constant term 
-let accountName = "Sougata" //mostly used to declare a variable
-var accountEmail = "smondal@gmail.com"  //old way to declare a variable
-accountPassword = 8932 //will take it as a variable, but bad practice
-
-console.log(accountID)
-console.table([accountEmail, accountID, accountName, accountPassword])
-
