196a7462b791ce0595034ec29092a27b5e56d8a1
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-09-24 06:45:06 +0000

    done

--- 05_iterations/obj_adv.js
+++ 05_iterations/obj_adv.js
@@ -15,3 +15,15 @@ for (const key in programming) {
     console.log(key); //will print the keys ie. the index
     
 }
+
+const map = new Map() //Map is an object that holds key value pair, key in the map is unique
+map.set('IN', "India")
+map.set('US', "United States")
+map.set('FR', "France")
+map.set('IT', "Italy")
+map.set('IN', "India")
+
+//will not print anything, cause map is not iterable
+for (const key in map) {
+    console.log(key);
+}
