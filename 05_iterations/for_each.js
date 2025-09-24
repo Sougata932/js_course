c2f2da5457a9234590aea523c489892656278ea8
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-09-24 08:28:25 +0000

    done

---
+++ 05_iterations/for_each.js
@@ -0,0 +1,10 @@
+const coding = ["js", "python", "cpp", "ruby"]
+
+coding.forEach( function(val){
+    console.log(val);  
+})
+ 
+//with arrow function
+coding.forEach((val) => {
+    console.log(val);
+});
