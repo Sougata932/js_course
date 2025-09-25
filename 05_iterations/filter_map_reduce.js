7c9fe56deadf4e827707872b1fb620eea1b5dbe5
Sougata <90645646+Sougata932@users.noreply.github.com>
2025-09-25 18:51:22 +0000

    done

--- 05_iterations/filter_map_reduce.js
+++ 05_iterations/filter_map_reduce.js
@@ -8,5 +8,20 @@ console.log(values); //prints undefined, cause forEach doesn't return any value
 
 const myNums = [1,2,3,4,5,6,7]
 
-const newNums=myNums.filter((num)=> num>4)
-console.log(newNums);
+const newNums = myNums.filter((num)=> num>4)
+console.log(newNums); //this returns value
+//OR
+const num =myNums.filter((i)=>{
+    return i>4 //if we use {} then we have to return the value seperately
+})
+console.log(num); 
+
+//with foreach
+const new_Nums = []
+myNums.forEach((num)=>{
+    if(num > 4){
+        new_Nums.push(num)
+    }
+})
+console.log(new_Nums);
+
